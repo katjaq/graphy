34caf8b3945f7fe1bf3a4d4fa2331de48a88953c
katjaq <katjaqheuer@gmail.com>
2020-01-03 17:52:21 +0100

    embed art of MR youtube video

--- js/app/tomo.js
+++ js/app/tomo.js
@@ -49,7 +49,26 @@ define(["jquery", "misc"], function($, misc) {
         }).mouseleave(function(e) {
 			e.preventDefault();
             return false;
-        });
+	    });
+
+	// 	$('a[href="#instal"]').click(function(e) {
+	// 	e.preventDefault();
+	// 	misc.showContentDiv( 'contentDiv3' );
+	// 	$("#video3")[0].pause();
+	// 	$("#video4")[0].pause();
+	// 	var $ytdiv = $( '<div />' );
+	// 	$ytdiv.attr( 'id', 'ytplayer' );
+	// 	$( '#contentDiv3' ).append($ytdiv);
+	// 	misc.startYT( 'ytplayer', 'LtsBZlw5nQI' );
+	// 	return false;
+	// }).mouseenter(function(e) {
+	// 	e.preventDefault();
+	// 	return false;
+	// }).mouseleave(function(e) {
+	// 	e.preventDefault();
+	// 	return false;
+	// });
+
 	$('a[href="#arssub"]').click(function(e) {
 			e.preventDefault();
 			misc.showContentDiv( 'contentDiv4' );
@@ -64,7 +83,25 @@ define(["jquery", "misc"], function($, misc) {
 			e.preventDefault();
             return false;
         });
-		
+
+	$('a[href="#artofmr"]').click(function(e) {
+			e.preventDefault();
+			misc.showContentDiv( 'contentDiv4' );
+			$("#video3")[0].pause();
+			$("#video4")[0].pause();
+			var $ytdiv = $( '<div />' );
+			$ytdiv.attr( 'id', 'ytplayer2' );
+			$( '#contentDiv1' ).append($ytdiv);
+			misc.startYT( 'ytplayer2', 'LtsBZlw5nQI' );
+			return false;
+		}).mouseenter(function(e) {
+			e.preventDefault();
+			return false;
+		}).mouseleave(function(e) {
+			e.preventDefault();
+			return false;
+		});
+
 		
 	$('a[href="#inter"]').click(function(e) {
 			e.preventDefault();
--- tomography/app.js
+++ tomography/app.js
@@ -19,6 +19,7 @@ require(['jquery', 'misc', 'ui', 'io', 'gfx/viewer', 'gfx/mygl', 'html5slider'],
 				e.preventDefault();
 				misc.showContentDiv( 'contentDiv1' );
 				$("#video3")[0].pause();
+				$( '#contentDiv4' ).empty();
 				var $ytdiv = $( '<div />' );
 				$ytdiv.attr( 'id', 'ytplayer' );
 				$( '#contentDiv1' ).append($ytdiv);
@@ -36,6 +37,7 @@ require(['jquery', 'misc', 'ui', 'io', 'gfx/viewer', 'gfx/mygl', 'html5slider'],
 					e.preventDefault();
 					misc.showContentDiv( 'contentDiv2' );
 					$( '#contentDiv1' ).empty();
+					$( '#contentDiv4' ).empty();
 					$("#video3")[0].pause();
 					return false;
 				}).mouseenter(function(e) {
@@ -56,7 +58,8 @@ require(['jquery', 'misc', 'ui', 'io', 'gfx/viewer', 'gfx/mygl', 'html5slider'],
 					e.preventDefault();
 					misc.showContentDiv( 'contentDiv3' );
 					$( '#contentDiv1' ).empty();
-					$("#video4")[0].pause();
+					// $("#video4")[0].pause();
+					$( '#contentDiv4' ).empty();
 					$("#video3")[0].play();
 					return false;
 				}).mouseenter(function(e) {
@@ -67,26 +70,45 @@ require(['jquery', 'misc', 'ui', 'io', 'gfx/viewer', 'gfx/mygl', 'html5slider'],
 					return false;
 				});
 
+			/* Playing video from local storage */
+			// $('a[href="#artofmr"]').click(function(e) {
+			// 		e.preventDefault();
+			// 		misc.showContentDiv( 'contentDiv4' );
+			// 		$( '#contentDiv1' ).empty();
+			// 		$("#video4")[0].play();
+			// 		$("#video3")[0].pause();
+			// 		return false;
+			// 	}).mouseenter(function(e) {
+			// 		e.preventDefault();
+			// 		return false;
+			// 	}).mouseleave(function(e) {
+			// 		e.preventDefault();
+			// 		return false;
+			// 	});
 			$('a[href="#artofmr"]').click(function(e) {
-					e.preventDefault();
-					misc.showContentDiv( 'contentDiv4' );
-					$( '#contentDiv1' ).empty();
-					$("#video4")[0].play();
-					$("#video3")[0].pause();
-					return false;
-				}).mouseenter(function(e) {
-					e.preventDefault();
-					return false;
-				}).mouseleave(function(e) {
-					e.preventDefault();
-					return false;
-				});				
-				
+				e.preventDefault();
+				misc.showContentDiv( 'contentDiv4' );
+				$( '#contentDiv1' ).empty();
+				$("#video3")[0].pause();
+				var $ytdiv = $( '<div />' );
+				$ytdiv.attr( 'id', 'ytplayer2' );
+				$( '#contentDiv4' ).append($ytdiv);
+				misc.startYT( 'ytplayer2', 'LtsBZlw5nQI' );
+				return false;
+			}).mouseenter(function(e) {
+				e.preventDefault();
+				return false;
+			}).mouseleave(function(e) {
+				e.preventDefault();
+				return false;
+			});
+
 			$('a[href="#inter"]').click(function(e) {
 					e.preventDefault();
 					misc.showContentDiv( 'viewer-div' );
 					$( '#contentDiv1' ).empty();
 					$("#video3")[0].pause();
+					$( '#contentDiv4' ).empty();
 					return false;
 				}).mouseenter(function(e) {
 					e.preventDefault();
@@ -99,8 +121,9 @@ require(['jquery', 'misc', 'ui', 'io', 'gfx/viewer', 'gfx/mygl', 'html5slider'],
 			$('a[href="#unfolding"]').click(function(e) {
 				    e.preventDefault();
 				    $( '#contentDiv1' ).empty();
-				    $("#video3")[0].pause();
-				    $("#video4")[0].pause();
+				    $( "#video3" )[0].pause();
+					// $("#video4")[0].pause();
+					$( '#contentDiv4' ).empty();
 				    misc.showImg( '#img001' );
 				    misc.showContentDiv( 'contentDiv5' );
 				    return false;
